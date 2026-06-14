38607ead66916ec8a842d3601e50b27305e94efc
jay81k <jaykrief@yahoo.ca>
2026-06-14 14:16:12 -0400

    Adjust chart height and margin in market.js

--- js/market.js
+++ js/market.js
@@ -242,7 +242,7 @@
                 '</div>' +
             '</div>' +
             subRowHtml +
-            '<div class="mic-chart-wrap" id="' + chartContainerId + '" style="height:220px;margin:4px 0 2px;"></div>';
+            '<div class="mic-chart-wrap" id="' + chartContainerId + '" style="height:180px;margin:2px 0 2px;"></div>';
 
         // Render LightweightCharts into the placeholder now that it's in the DOM
         marketRenderIndexChart(document.getElementById(chartContainerId), parsed, direction);
